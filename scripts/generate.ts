5348665ef063ec3b368740fb7524796a18e91afb
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-02-06 17:42:24 +0300

    feat: finally... code-generation

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -10,15 +10,19 @@ const schema = JSON.parse(String(schemaFile)) as IBotApi.ISchema;
 const methods: Partial<
 	Record<
 		keyof ApiMethods,
-		{ name: string; type?: "array"; property?: string }[]
+		{ name: string; type?: "array" | "union"; property?: string }[]
 	>
 > = {};
 
 function resolveReference(name: string) {
 	const telegramObject = schema.objects.find((x) => x.name === name);
 
-	if (!telegramObject) return [];
-	return telegramObject?.properties as IBotApi.IArgument[];
+	if (!telegramObject) return [false, false] as const;
+	return [
+		(telegramObject?.properties ||
+			telegramObject.any_of) as IBotApi.IArgument[],
+		"properties" in telegramObject ? "properties" : "any_of",
+	] as const;
 }
 
 function findInputFileInArguments(
@@ -29,7 +33,6 @@ function findInputFileInArguments(
 	if (!methodArguments?.length) return [];
 
 	for (const argument of methodArguments) {
-		if (!argument.name) console.log(argument);
 		if (argument.reference === "InputFile")
 			fileArguments.push({
 				name: argumentName ?? argument.name,
@@ -40,10 +43,18 @@ function findInputFileInArguments(
 				...findInputFileInArguments(argument.any_of, argument.name),
 			);
 
-		if (argument.reference && argument.reference !== "InputFile")
-			fileArguments.push(
-				...findInputFileInArguments(resolveReference(argument.reference)),
-			);
+		if (argument.reference && argument.reference !== "InputFile") {
+			const [referenceArguments, type] = resolveReference(argument.reference);
+
+			if (referenceArguments || type)
+				fileArguments.push(
+					...findInputFileInArguments(referenceArguments).map((x) => ({
+						name: x.name,
+						property: type === "any_of" ? argument.name : undefined,
+						type: type === "any_of" ? ("union" as const) : undefined,
+					})),
+				);
+		}
 
 		if (argument.array)
 			fileArguments.push(
@@ -63,38 +74,61 @@ function findInputFileInArguments(
 
 for (const method of schema.methods) {
 	if (method.multipart_only && method.arguments?.length) {
+		if (method.name === "editMessageMedia")
+			console.log(
+				findInputFileInArguments(method.arguments),
+				resolveReference("InputMedia"),
+			);
+		// [INFO] Find only unique values (inspired by https://yagisanatode.com/get-a-unique-list-of-objects-in-an-array-of-object-in-javascript/)
 		methods[method.name] = [
-			...new Set(findInputFileInArguments(method.arguments)),
+			...new Map(
+				findInputFileInArguments(method.arguments).map((item) => [
+					item.name,
+					item,
+				]),
+			).values(),
 		];
 	}
 }
-console.log(methods);
 
 fs.writeFile(
 	"./src/test.ts",
-	// await prettier.format(
-	`import { ApiMethods } from "@gramio/types";
+	await prettier.format(
+		`import { ApiMethods, TelegramInputFile } from "@gramio/types";
+
     type MethodsWithMediaUpload = {
         [Method in keyof ApiMethods]?: (params: (NonNullable<
             Parameters<ApiMethods[Method]>[0]
-        >)) => any;
+        >)) => boolean;
     };
+
+	function isFile(file?: TelegramInputFile | string) {
+		if(!file || typeof file === "string") return false;
+
+		return file instanceof File;
+	}
+
     export const MEDIA_METHODS: MethodsWithMediaUpload = {${Object.entries(
 			methods,
 		)
 			.map(([key, value]) => {
-				// TODO:
-				if (!value.length) return "";
-
 				return `${key}: (params) => ${value
 					.map((x) =>
 						x.type === "array"
-							? `params.${x.property}.some(x => x.${x.name})`
-							: `params.${x.name}`,
+							? `params.${x.property}.some(x => "${x.name}" in x && isFile(x.${x.name}))`
+							: `${
+									x.type === "union"
+										? `"${x.name}" in params${
+												x.property ? `.${x.property}` : ""
+										  } && `
+										: ""
+							  }isFile(params.${
+									x.property ? `${x.property}.${x.name}` : `${x.name}`
+							  })`,
 					)
 					.join(" || ")},`;
 			})
 			.join("\n")}}`,
-	// { tabWidth: 4, parser: "typescript", endOfLine: "auto", semi: false },
-	// ),
+		{ tabWidth: 4, parser: "typescript", endOfLine: "auto", semi: false },
+	),
 );
--- src/test.ts
+++ src/test.ts
@@ -1,22 +1,37 @@
-import { ApiMethods } from "@gramio/types";
-    type MethodsWithMediaUpload = {
-        [Method in keyof ApiMethods]?: (params: (NonNullable<
-            Parameters<ApiMethods[Method]>[0]
-        >)) => any;
-    };
-    export const MEDIA_METHODS: MethodsWithMediaUpload = {setWebhook: (params) => params.certificate,
-sendPhoto: (params) => params.photo,
-sendAudio: (params) => params.audio || params.thumbnail,
-sendDocument: (params) => params.document || params.thumbnail,
-sendVideo: (params) => params.video || params.thumbnail,
-sendAnimation: (params) => params.animation || params.thumbnail,
-sendVoice: (params) => params.voice,
-sendVideoNote: (params) => params.video_note || params.thumbnail,
-sendMediaGroup: (params) => params.media.some(x => x.thumbnail) || params.media.some(x => x.thumbnail) || params.media.some(x => x.thumbnail),
-setChatPhoto: (params) => params.photo,
+import { ApiMethods, TelegramInputFile } from "@gramio/types";
 
-sendSticker: (params) => params.sticker,
-uploadStickerFile: (params) => params.sticker,
-createNewStickerSet: (params) => params.stickers.some(x => x.sticker),
-addStickerToSet: (params) => params.sticker,
-setStickerSetThumbnail: (params) => params.thumbnail,}
+type MethodsWithMediaUpload = {
+	[Method in keyof ApiMethods]?: (
+		params: NonNullable<Parameters<ApiMethods[Method]>[0]>,
+	) => boolean;
+};
+
+function isFile(file?: TelegramInputFile | string) {
+	if (!file || typeof file === "string") return false;
+
+	return file instanceof File;
+}
+
+export const MEDIA_METHODS: MethodsWithMediaUpload = {
+	setWebhook: (params) => isFile(params.certificate),
+	sendPhoto: (params) => isFile(params.photo),
+	sendAudio: (params) => isFile(params.audio) || isFile(params.thumbnail),
+	sendDocument: (params) => isFile(params.document) || isFile(params.thumbnail),
+	sendVideo: (params) => isFile(params.video) || isFile(params.thumbnail),
+	sendAnimation: (params) =>
+		isFile(params.animation) || isFile(params.thumbnail),
+	sendVoice: (params) => isFile(params.voice),
+	sendVideoNote: (params) =>
+		isFile(params.video_note) || isFile(params.thumbnail),
+	sendMediaGroup: (params) =>
+		params.media.some((x) => "thumbnail" in x && isFile(x.thumbnail)),
+	setChatPhoto: (params) => isFile(params.photo),
+	editMessageMedia: (params) =>
+		"thumbnail" in params.media && isFile(params.media.thumbnail),
+	sendSticker: (params) => isFile(params.sticker),
+	uploadStickerFile: (params) => isFile(params.sticker),
+	createNewStickerSet: (params) =>
+		params.stickers.some((x) => "sticker" in x && isFile(x.sticker)),
+	addStickerToSet: (params) => isFile(params.sticker),
+	setStickerSetThumbnail: (params) => isFile(params.thumbnail),
+};
