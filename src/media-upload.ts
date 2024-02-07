bc5ccc3126e96a2b6629ca153749f58bec36bb69
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-02-07 17:25:39 +0300

    feat: add MediaUpload helper

---
+++ src/media-upload.ts
@@ -0,0 +1,38 @@
+import fs from "node:fs/promises";
+import { basename } from "node:path";
+import { Readable } from "node:stream";
+import { convertStreamToBuffer } from "./utils";
+
+export class MediaUpload {
+	static async path(path: string, filename?: string) {
+		const buffer = await fs.readFile(path);
+
+		return new File([buffer], filename ?? basename(path));
+	}
+
+	static async stream(stream: Readable, filename: string) {
+		const buffer = await convertStreamToBuffer(stream);
+
+		return new File([buffer], filename);
+	}
+
+	static async buffer(buffer: Buffer | ArrayBuffer, filename: string) {
+		return new File([buffer], filename);
+	}
+
+	static async url(
+		url: URL | string,
+		filename?: string,
+		options?: RequestInit,
+	) {
+		const res = await fetch(url, options);
+
+		const buffer = await res.arrayBuffer();
+
+		return new File(
+			[buffer],
+			filename ??
+				(typeof url === "string" ? basename(url) : basename(url.pathname)),
+		);
+	}
+}
